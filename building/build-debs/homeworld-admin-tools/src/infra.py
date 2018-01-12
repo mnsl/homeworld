c2667fd124369460faf46bdf77d5c6d2dbf9fadc
J. Maunsell <maunsell@mit.edu>
2018-01-11 19:22:09 -0500

    Add infra run-all cmd for batching cmds across nodes

--- building/build-debs/homeworld-admin-tools/src/infra.py
+++ building/build-debs/homeworld-admin-tools/src/infra.py
@@ -31,8 +31,19 @@ def infra_install_packages(ops: setup.Operations) -> None:
         ops.ssh("upgrade packages on @HOST", node, "apt-get", "upgrade", "-y")
 
 
+def infra_run_all(ops: setup.Operations, kind: str, *script: str) -> None:
+    kinds = ["master", "supervisor", "worker", "all"]
+    if kind not in kinds:
+        fail("must specify kind of node to run on: master, supervisor, worker, or all")
+    config = configuration.get_config()
+    for node in config.nodes:
+        if kind == "all" or node.kind == kind:
+            ops.ssh("run cmd on @HOST", node, *script)
+
+
 main_command = command.mux_map("commands about maintaining the infrastructure of a cluster", {
     "admit": command.wrap("request a token to admit a node to the cluster", infra_admit),
     "admit-all": command.wrap("request tokens to admit every non-supervisor node to the cluster", infra_admit_all),
     "install-packages": setup.wrapop("install and update packages on a node", infra_install_packages),
+    "run-all": setup.wrapop("run cmd on all nodes of specified kind", infra_run_all),
 })
